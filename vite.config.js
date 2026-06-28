a695bb4407ceb367563698096acce1361b99728e
whjin <wuhuajin09@163.com>
2026-06-29 07:20:42 +0800

    提交更新

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     base: mode === 'production' ? '/' : './',
